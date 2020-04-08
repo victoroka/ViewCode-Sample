6c78d5d3d6ade3115b4e0b59ba7e75bfcac7c340
Victor Hideo Oka <oka.victor97@gmail.com>
2020-04-07 23:52:26 -0300

    setup root view controller

--- ViewCode-Sample/ViewCode-Sample/ViewController.swift
+++ ViewCode-Sample/ViewCode-Sample/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .red
+        self.view = view
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
